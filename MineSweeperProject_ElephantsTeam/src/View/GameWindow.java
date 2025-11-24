7677a6c9bda3824c7d4f197b2b8a5be1d791557f
weaamsh13 <shehadeweaam@gmail.com>
2025-11-24 14:14:51 +0200

    Changed Diffuculty to Hard

--- MineSweeperProject_ElephantsTeam/src/View/GameWindow.java
+++ MineSweeperProject_ElephantsTeam/src/View/GameWindow.java
@@ -144,6 +144,6 @@ public class GameWindow extends JFrame {
     }
     
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GameWindow(Difficulty.MEDIUM));
+        SwingUtilities.invokeLater(() -> new GameWindow(Difficulty.HARD));
     }
 }
